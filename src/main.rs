263805736214d26e5b3ef2bdc5eca42ffaf1551e
Paul Hultgren <pjhultgren5@gmail.com>
2023-04-30 12:34:01 -0700

    refactor: move main code into new server file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use std::{
-    env,
-    io::Error,
-};
+use std::env;
 
-use futures_util::{future, pin_mut, StreamExt, TryStreamExt};
-use log::{info, warn};
-use serde::Deserialize;
-use tokio::net::{TcpListener, TcpStream};
-
-use synapse::channel::ChannelMap;
-use tokio_tungstenite::tungstenite::Message;
-
-#[derive(Deserialize)]
-struct MessageBody {
-    channel: String,
-    payload: serde_json::Value,
-}
+use synapse::server::Server;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
     env_logger::init_from_env(env);
 
@@ -26,58 +11,8 @@ async fn main() -> Result<(), Error> {
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let channels = ChannelMap::new();
-
-    // Create the event loop and TCP listener we'll accept connections on.
-    let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
-    info!("Listening on: {}", addr);
-
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(accept_connection(channels.clone(), stream));
-    }
+    let server = Server::new();
+    server.run(&addr).await;
 
     Ok(())
-}
-
-async fn accept_connection(channels: ChannelMap, stream: TcpStream) {
-    let addr = stream
-        .peer_addr()
-        .expect("connected streams should have a peer address");
-    info!("Incoming connection: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
-
-    let (sender, receiver) = futures_channel::mpsc::unbounded();
-
-    let (write, read) = ws_stream.split();
-
-    let broadcast_incoming = read.try_for_each(|msg| {
-        info!("Received a message from {}: {}", addr, msg);
-
-        if msg.is_text() {
-            let msg = msg.to_text().unwrap();
-
-            // TODO: Handle error better
-            let body: MessageBody = serde_json::from_str(msg).unwrap();
-
-            channels.add_channel(body.channel.clone());
-            channels.add_connection(body.channel.clone(), addr, sender.clone());
-            channels.broadcast(body.channel.clone(), addr, Message::text(msg));
-        } else {
-            warn!("Received a non-text message from {}: {}", addr, msg);
-        }
-
-        future::ok(())
-    });
-
-    let receive_from_others = receiver.map(Ok).forward(write);
-
-    pin_mut!(broadcast_incoming, receive_from_others);
-    future::select(broadcast_incoming, receive_from_others).await;
-
-    // TODO: Remove connection from all channels
-    // connections.lock().unwrap().remove(&addr);
 }
---
+++ src/server.rs
@@ -0,0 +1,83 @@
+use std::sync::Arc;
+
+use futures_util::{future, pin_mut, StreamExt, TryStreamExt};
+use log::{info, warn};
+use serde::Deserialize;
+use tokio::net::{TcpListener, TcpStream};
+
+use crate::channel::ChannelMap;
+use tokio_tungstenite::tungstenite::Message;
+
+#[derive(Deserialize)]
+struct MessageBody {
+    channel: String,
+    payload: serde_json::Value,
+}
+
+pub struct Server {
+    channels: Arc<ChannelMap>,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Server {
+            channels: Arc::new(ChannelMap::new()),
+        }
+    }
+
+    pub async fn run(self, addr: &str) {
+        let try_socket = TcpListener::bind(&addr).await;
+        let listener = try_socket.expect("Failed to bind");
+        info!("Listening on: {}", addr);
+
+        while let Ok((stream, _)) = listener.accept().await {
+            let channels = self.channels.clone();
+
+            tokio::spawn(async move {
+                Server::accept_connection(channels, stream).await;
+            });
+        }
+    }
+
+    async fn accept_connection(channels: Arc<ChannelMap>, stream: TcpStream) {
+        let addr = stream
+            .peer_addr()
+            .expect("connected streams should have a peer address");
+        info!("Incoming connection: {}", addr);
+
+        let ws_stream = tokio_tungstenite::accept_async(stream)
+            .await
+            .expect("Error during the websocket handshake occurred");
+
+        let (sender, receiver) = futures_channel::mpsc::unbounded();
+
+        let (write, read) = ws_stream.split();
+
+        let broadcast_incoming = read.try_for_each(|msg| {
+            info!("Received a message from {}: {}", addr, msg);
+
+            if msg.is_text() {
+                let msg = msg.to_text().unwrap();
+
+                // TODO: Handle error better
+                let body: MessageBody = serde_json::from_str(msg).unwrap();
+
+                channels.add_channel(body.channel.clone());
+                channels.add_connection(body.channel.clone(), addr, sender.clone());
+                channels.broadcast(body.channel.clone(), addr, Message::text(msg));
+            } else {
+                warn!("Received a non-text message from {}: {}", addr, msg);
+            }
+
+            future::ok(())
+        });
+
+        let receive_from_others = receiver.map(Ok).forward(write);
+
+        pin_mut!(broadcast_incoming, receive_from_others);
+        future::select(broadcast_incoming, receive_from_others).await;
+
+        // TODO: Remove connection from all channels
+        // connections.lock().unwrap().remove(&addr);
+    }
+}
