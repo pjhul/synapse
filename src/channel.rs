b4ccebcb9eb4273f3f9aa51845a3e2777f0a13b1
Paul Hultgren <pjhultgren5@gmail.com>
2023-04-29 23:41:53 -0700

    add helper methods

--- src/channel.rs
+++ src/channel.rs
@@ -27,16 +27,39 @@ impl ChannelMap {
 
     pub fn add_channel(&self, name: String) {
         let mut channels = self.channels.lock().unwrap();
-        channels.insert(name.clone(), Channel {
-            name,
-            connections: Arc::new(Mutex::new(HashMap::new())),
-        });
+
+        channels.insert(
+            name.clone(),
+            Channel {
+                name,
+                connections: Arc::new(Mutex::new(HashMap::new())),
+            },
+        );
     }
 
     pub fn add_connection(&self, channel_name: String, addr: SocketAddr, sender: Sender) {
         let mut channels = self.channels.lock().unwrap();
         let channel = channels.get_mut(&channel_name).unwrap();
+
         channel.connections.lock().unwrap().insert(addr, sender);
+    }
+
+    pub fn remove_connection(&self, addr: SocketAddr) {
+        let mut channels = self.channels.lock().unwrap();
+
+        for (_, channel) in channels.iter_mut() {
+            channel.connections.lock().unwrap().remove(&addr);
+        }
+    }
+
+    pub fn send_message(&self, channel_name: String, message: Message) {
+        let channels = self.channels.lock().unwrap();
+        let channel = channels.get(&channel_name).unwrap();
+        let connections = channel.connections.lock().unwrap();
+
+        for (_, sender) in connections.iter() {
+            sender.unbounded_send(message.clone()).unwrap();
+        }
     }
 }
 
