3707524e90ac4f2d543f7e464fa56d1fdfc59c0f
Paul Hultgren <pjhultgren5@gmail.com>
2023-04-29 22:06:47 -0700

    feat: display info logs

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ type ConnectionMap = Arc<Mutex<HashMap<SocketAddr, Sender>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let _ = env_logger::try_init();
+    let env = env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info");
+    env_logger::init_from_env(env);
+
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
     let connections = ConnectionMap::new(Mutex::new(HashMap::new()));
