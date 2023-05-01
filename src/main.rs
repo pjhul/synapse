dc8e1d80e3459000f50f59ba7c1fa077af76ad3d
Paul Hultgren <pjhultgren5@gmail.com>
2023-04-30 20:51:22 -0700

    fix: listen on 0.0.0.0 by default

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let addr = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+        .unwrap_or_else(|| "0.0.0.0:8080".to_string());
 
     let server = Server::new();
     server.run(&addr).await;
